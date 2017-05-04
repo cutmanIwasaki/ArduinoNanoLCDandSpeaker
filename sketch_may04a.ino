d2712ded1f96307afa31d8d89b4d4d2dc8d8ad79
cutmaniwasaki <Cutman.c5h12@gmail.com>
2017-05-04 15:47:23 +0900

    LCD_str関数を追加

--- sketch_may04a.ino
+++ sketch_may04a.ino
@@ -41,19 +41,29 @@ delay(20);
 writeCommand(0x0C);
 delay(20);
 }
+//LCDへ文字列送信
+void LCD_str(char *c) {
+  unsigned char i,wk;
+  for (i=0 ; ; i++) {
+    wk = c[i];
+    if  (wk == 0x00) {break;}
+    writeData(wk);
+  }
+
+}
 
 void setup() {
 Wire.begin();
 init_LCD();
 }
 void loop() {
- for(int i = 0; i < 16; i++) {
- writeData(moji[i]);
- }
-writeCommand(0x40+0x80); // 2LINE TOP
- for(int i = 0; i < 16; i++) {
- writeData(i+0xb1);
- }
-while(1){}//stop
+  LCD_str(moji);
+  
+  writeCommand(0x40+0x80); // 2LINE TOP
+  for(int i = 0; i < 16; i++) {
+  writeData(i+0xb1);
+  }
+  
+  while(1){}//stop
 } 
 
